ef8f54e773eda8a768cda71337d1d84ea8697317
Vanshika Asthana <24csu229@ncuindia.edu>
2026-02-03 12:53:58 +0530

    Add background image and fix UI layout

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ def add_bg_from_local(image_file):
         unsafe_allow_html=True
     )
 
-add_bg_from_local("app/assets/bg.jpg")
+add_bg_from_local("app/assets/bg1.jpg")
 st.markdown("""
 <style>
 .main-container {
